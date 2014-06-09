874868ffcccdf160e9617aa13ee8f2552e9e3937
Cliffmeister <jaycliff@users.noreply.github.com>
2014-06-09 16:50:00 +0800

    Update setgameloopcallback.js

--- setgameloopcallback.js
+++ setgameloopcallback.js
@@ -1,5 +1,5 @@
 /*
-    Copyright 2013 Jaycliff Arcilla
+    Copyright 2014 Jaycliff Arcilla
     
     Licensed under the Apache License, Version 2.0 (the "License");
     you may not use this file except in compliance with the License.
