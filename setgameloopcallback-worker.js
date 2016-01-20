5bded33a69c2df29d5479a3ff931a5bf7f8283e1
Cliffmeister <jaycliff.a@agentsofvalue.com>
2016-01-20 14:38:52 +0800

    Minor update

--- setgameloopcallback-worker.js
+++ setgameloopcallback-worker.js
@@ -1,22 +1,25 @@
-"use strict";
-var interval_id, is_looping = false;
-function loopStepper() {
-    self.postMessage('loop step');
-}
-self.onmessage = function (event) {
-    var data = event.data;
-    switch (data) {
-    case 'start':
-        if (!is_looping) {
-            interval_id = setInterval(loopStepper, 1000 / 60);
-            is_looping = true;
-        }
-        break;
-    case 'stop':
-        if (is_looping) {
-            clearInterval(interval_id);
-            is_looping = false;
-        }
-        break;
+/*global self*/
+(function setGameLoopCallbackWorkerSetup() {
+    "use strict";
+    var interval_id, is_looping = false;
+    function loopStepper() {
+        self.postMessage("loop step");
     }
-};
+    self.onmessage = function (event) {
+        var data = event.data;
+        switch (data) {
+        case "start":
+            if (!is_looping) {
+                interval_id = setInterval(loopStepper, 1000 / 60);
+                is_looping = true;
+            }
+            break;
+        case "stop":
+            if (is_looping) {
+                clearInterval(interval_id);
+                is_looping = false;
+            }
+            break;
+        }
+    };
+}());
